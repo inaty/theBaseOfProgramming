61978fd471bfe134c794092d3363f57fd3216c46
inaty <inaty0610@gmail.com>
2021-12-13 23:55:04 +0900

    delete already exists files

--- exe/exe5/hanbetsushiki.ml
+++
@@ -1,8 +0,0 @@
-(* 目的：2次方程式 a*(x**2) + b * x + c = 0 の係数a,b,c(いずれも実数)を与えられたら、判別式を返す。aは0でない。 *)
-(* hanbetsusiki : float -> float -> float -> float *)
-let hanbetsusiki a b c = b ** 2.0 -. (4.0 *. a *. c)
-
-(* テスト *)
-let test1 = hanbetsusiki 1.0 0.0 1.0 = -. 4.0
-let test2 = hanbetsusiki 0.0 1.0 0.0 = 1.0
-let test3 = hanbetsusiki 1.0 1.0 1.0 = -. 3.0
